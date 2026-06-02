1dae899d4467f1e460082ff972f3563e7fcc7ec3
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-02 17:24:12 +0200

    profile Card component updated

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,11 +1,26 @@
+import "../styles/ProfileCard.css";
 
 function ProfileCard(props) {
     return (
-        <div>
+        <div className="profile-card">
+
+            <img
+                className="profile-image"
+                src="https://upload.wikimedia.org/wikipedia/commons/d/d3/Robert_Downey%2C_Jr._2012.jpg"
+                alt="Software Developer"
+            />
+
             <h2>{props.name}</h2>
             <p>{props.role}</p>
+
+            <div className="skills">
+                <span className="skill">React.js</span>
+                <span className="skill">Node.js</span>
+                <span className="skill">HTML</span>
+                <span className="skill">CSS</span>
+            </div>
         </div>
     );
 }
 
-export default ProfileCard; 
+export default ProfileCard;
